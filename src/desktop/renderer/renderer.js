60f87f8284610068dcf1d20f2ad2b736755679a7
caio da silva <caio@debian.caio>
2026-07-01 08:52:55 -0300

    chore: bump version to 12.0.7

--- src/desktop/renderer/renderer.js
+++ src/desktop/renderer/renderer.js
@@ -4232,7 +4232,7 @@ async function init() {
       elements.iconPreview.src = iconPreviewPath(state.defaultIconPath);
     }
   } catch {
-    elements.appVersion.textContent = "v12.0.5";
+    elements.appVersion.textContent = "v12.0.7";
   }
 
   setTimeout(finishBoot, 1800);
